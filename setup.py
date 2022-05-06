6a6cb9e6092dfe1a3f3ca8bac533c9a3a700dd85
Liam Wickins <liam@icoteq.com>
2022-05-06 14:04:37 +0100

    Bump timeout to 6 seconds

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pylinkit',
-    version='3.3.1',
+    version='3.3.2',
     description='Python LinkIt BLE configuration tool',
     author='Liam Wickins',
     author_email='liam@icoteq.com',
